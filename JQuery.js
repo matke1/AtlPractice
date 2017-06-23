cca5329b99f0fe40e76534c47320d73105a0aaff
matke1 <matke11.mm@gmail.com>
2017-06-23 12:08:17 +0200

    Osmi commit

--- JQuery.js
+++ JQuery.js
@@ -1,23 +1,3 @@
-//$(document).ready(function(){
-   // $(".openMenu").click(function(){
-    //    $(".navOver").slideDown(100);
-     //   $(".wrapper").slideUp(100);
-     //   $('.close').slideDown(100);
-     //   $('.dropDownLogo').slideDown(100);
-    //    $('.navOverlayFIT').slideDown(100);
-   // });
-//});
-//$(document).ready(function(){
-   // $(".close").click(function(){
-     //   $('.close').slideUp(300);
-    //    $('.dropDownLogo').slideUp(300);
-    //    $(".navOver").slideUp(300);
-    //    $(".wrapper").slideDown(300);
-    //    $('.navOverlayFIT').slideUp(300);
-   // });
-//});
-
-
 $(document).ready(function(){
     $(".openMenu").click(function(){
         $(".navOver").slideToggle(400);
