f52a20f132a975b37cef41a95c2f7d3a14d86027
matke1 <matke11.mm@gmail.com>
2017-06-23 12:07:08 +0200

    Sedmi commit

--- JQuery.js
+++ JQuery.js
@@ -1,18 +1,32 @@
+//$(document).ready(function(){
+   // $(".openMenu").click(function(){
+    //    $(".navOver").slideDown(100);
+     //   $(".wrapper").slideUp(100);
+     //   $('.close').slideDown(100);
+     //   $('.dropDownLogo').slideDown(100);
+    //    $('.navOverlayFIT').slideDown(100);
+   // });
+//});
+//$(document).ready(function(){
+   // $(".close").click(function(){
+     //   $('.close').slideUp(300);
+    //    $('.dropDownLogo').slideUp(300);
+    //    $(".navOver").slideUp(300);
+    //    $(".wrapper").slideDown(300);
+    //    $('.navOverlayFIT').slideUp(300);
+   // });
+//});
+
+
 $(document).ready(function(){
     $(".openMenu").click(function(){
-        $(".navOver").slideDown(100);
-        $(".wrapper").slideUp(50);
-        $('.close').slideDown(100);
-        $('.dropDownLogo').slideDown(100);
-        $('.navOverlayFIT').slideDown(100);
+        $(".navOver").slideToggle(400);
+        $(".wrapper").css("position","fixed");
     });
 });
 $(document).ready(function(){
     $(".close").click(function(){
-        $('.close').slideUp(100);
-        $('.dropDownLogo').slideUp(100);
-        $(".navOver").slideUp(100);
-        $(".wrapper").slideDown(50);
-        $('.navOverlayFIT').slideUp(100);
+        $(".navOver").slideToggle(400);
+        $(".wrapper").css("position","");
     });
 });
